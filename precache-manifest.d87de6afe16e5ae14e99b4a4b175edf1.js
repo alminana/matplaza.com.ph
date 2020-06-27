1997b91018990307ed68199b1d10d3fd60992f1d
alminana <alminanaeilerbutad@yahoo.com>
2020-06-27 09:09:11 -0700

    Updates

---
+++ precache-manifest.d87de6afe16e5ae14e99b4a4b175edf1.js
@@ -0,0 +1,194 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76c74eec3680a85cfbb8fa1131789c8d",
+    "url": "/http//alminana.github.io/matplaza.com.ph/index.html"
+  },
+  {
+    "revision": "ce23d6f986f6461dca7f",
+    "url": "/http//alminana.github.io/matplaza.com.ph/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "0917e22b77c9543d3c57",
+    "url": "/http//alminana.github.io/matplaza.com.ph/static/css/main.7d4a012d.chunk.css"
+  },
+  {
+    "revision": "ce23d6f986f6461dca7f",
+    "url": "/http//alminana.github.io/matplaza.com.ph/static/js/2.c63a813c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/http//alminana.github.io/matplaza.com.ph/static/js/2.c63a813c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0917e22b77c9543d3c57",
+    "url": "/http//alminana.github.io/matplaza.com.ph/static/js/main.0c9d7054.chunk.js"
+  },
+  {
+    "revision": "f42c61fb865eda6da417",
+    "url": "/http//alminana.github.io/matplaza.com.ph/static/js/runtime-main.fd995fdb.js"
+  },
+  {
+    "revision": "e114e2ec82c15d2509f0ff963f79af3e",
+    "url": "/http//alminana.github.io/matplaza.com.ph/static/media/ARCHITECT.e114e2ec.jpg"
+  },
+  {
+    "revision": "263875da0af1c8db2d4b106c984bec17",
+    "url": "/http//alminana.github.io/matplaza.com.ph/static/media/Furniture.263875da.gif"
+  },
+  {
+    "revision": "8919d259b408da5ef4922d7886179c3c",
+    "url": "/http//alminana.github.io/matplaza.com.ph/static/media/Furnituretop.8919d259.gif"
+  },
+  {
+    "revision": "4b00a2df8cbbc8eb37b210e16903d964",
+    "url": "/http//alminana.github.io/matplaza.com.ph/static/media/PRESIDENT.4b00a2df.JPG"
+  },
+  {
+    "revision": "c55fe90793423edafdac741414dbbb29",
+    "url": "/http//alminana.github.io/matplaza.com.ph/static/media/PVC.c55fe907.jpg"
+  },
+  {
+    "revision": "4d61a2f3ec7485be19ca5659458224e3",
+    "url": "/http//alminana.github.io/matplaza.com.ph/static/media/Samsung.4d61a2f3.jpg"
+  },
+  {
+    "revision": "2684680a307224d1edbcc163426b18a1",
+    "url": "/http//alminana.github.io/matplaza.com.ph/static/media/Sanitary.2684680a.JPG"
+  },
+  {
+    "revision": "01ee286c2d25fe7be91518273c357707",
+    "url": "/http//alminana.github.io/matplaza.com.ph/static/media/UPVC.01ee286c.jpg"
+  },
+  {
+    "revision": "94c0d5a96a46cebe33cc5d59db0a51e7",
+    "url": "/http//alminana.github.io/matplaza.com.ph/static/media/VICE.94c0d5a9.JPG"
+  },
+  {
+    "revision": "9d1e810cbed10440c7c2804292cc71cc",
+    "url": "/http//alminana.github.io/matplaza.com.ph/static/media/automation.9d1e810c.jpg"
+  },
+  {
+    "revision": "31d645dba72e5ef3cda0bcd1c0052b4c",
+    "url": "/http//alminana.github.io/matplaza.com.ph/static/media/banner.31d645db.JPG"
+  },
+  {
+    "revision": "4ee713d250a7a5cd8082afe947b27bb6",
+    "url": "/http//alminana.github.io/matplaza.com.ph/static/media/bayantreesoul.4ee713d2.jpg"
+  },
+  {
+    "revision": "37818295604d9686d50990e480f3d679",
+    "url": "/http//alminana.github.io/matplaza.com.ph/static/media/board.37818295.jpg"
+  },
+  {
+    "revision": "4dcf7f35c901f40af49ec26432d66c5a",
+    "url": "/http//alminana.github.io/matplaza.com.ph/static/media/boardcontent.4dcf7f35.gif"
+  },
+  {
+    "revision": "526d14a12de86e4e061b38b7606eeef8",
+    "url": "/http//alminana.github.io/matplaza.com.ph/static/media/boardtop.526d14a1.gif"
+  },
+  {
+    "revision": "06ab33ae3e2cbae0f509cd05f2ee6793",
+    "url": "/http//alminana.github.io/matplaza.com.ph/static/media/boxicons.06ab33ae.eot"
+  },
+  {
+    "revision": "5bbebd73a37de0cc242e872b4483269e",
+    "url": "/http//alminana.github.io/matplaza.com.ph/static/media/boxicons.5bbebd73.woff"
+  },
+  {
+    "revision": "5e99edcb80abc73c7a06aacfddca51bf",
+    "url": "/http//alminana.github.io/matplaza.com.ph/static/media/boxicons.5e99edcb.svg"
+  },
+  {
+    "revision": "72589e162de78952d4d6b5ce8b832175",
+    "url": "/http//alminana.github.io/matplaza.com.ph/static/media/boxicons.72589e16.ttf"
+  },
+  {
+    "revision": "aeaf4ee6e2c712532f982c8a4dd68b16",
+    "url": "/http//alminana.github.io/matplaza.com.ph/static/media/boxicons.aeaf4ee6.woff2"
+  },
+  {
+    "revision": "69c0f682dc6423b548e0eaf09d4c2f68",
+    "url": "/http//alminana.github.io/matplaza.com.ph/static/media/bulb.69c0f682.png"
+  },
+  {
+    "revision": "07d008a83514d874acd6541e29aa8852",
+    "url": "/http//alminana.github.io/matplaza.com.ph/static/media/eye.07d008a8.png"
+  },
+  {
+    "revision": "b615802b97f9fb91be54ab017c9a3934",
+    "url": "/http//alminana.github.io/matplaza.com.ph/static/media/fired.b615802b.jpg"
+  },
+  {
+    "revision": "638361fefae8fa87abc308af24ec770b",
+    "url": "/http//alminana.github.io/matplaza.com.ph/static/media/firedoor.638361fe.JPG"
+  },
+  {
+    "revision": "31d645dba72e5ef3cda0bcd1c0052b4c",
+    "url": "/http//alminana.github.io/matplaza.com.ph/static/media/furniture.31d645db.jpg"
+  },
+  {
+    "revision": "fb033ec087ef81657e0e4e5f9051c226",
+    "url": "/http//alminana.github.io/matplaza.com.ph/static/media/home_automation.fb033ec0.jpg"
+  },
+  {
+    "revision": "a34d7dfdcce7331dc528b60eb0bd4567",
+    "url": "/http//alminana.github.io/matplaza.com.ph/static/media/huosung.a34d7dfd.JPG"
+  },
+  {
+    "revision": "50a4ab76e700a83e649be213f820fbbd",
+    "url": "/http//alminana.github.io/matplaza.com.ph/static/media/icofont.50a4ab76.woff2"
+  },
+  {
+    "revision": "f6ab04aed30a8643bf94fe00f7ff0b59",
+    "url": "/http//alminana.github.io/matplaza.com.ph/static/media/icofont.f6ab04ae.woff"
+  },
+  {
+    "revision": "69f7788ef92b3a24603ca881fa88eeeb",
+    "url": "/http//alminana.github.io/matplaza.com.ph/static/media/office.69f7788e.jpg"
+  },
+  {
+    "revision": "627854ebfe6a989518821713abf27e9b",
+    "url": "/http//alminana.github.io/matplaza.com.ph/static/media/sanitarycontent.627854eb.jpg"
+  },
+  {
+    "revision": "91ecccc41c5c16e6764cb5c801b50cb8",
+    "url": "/http//alminana.github.io/matplaza.com.ph/static/media/stone.91ecccc4.jpg"
+  },
+  {
+    "revision": "c6f2685568f4b398fb3d1902a0b311ad",
+    "url": "/http//alminana.github.io/matplaza.com.ph/static/media/stonecontent.c6f26855.gif"
+  },
+  {
+    "revision": "79ea3a2e0c5e64501ed71198060516b0",
+    "url": "/http//alminana.github.io/matplaza.com.ph/static/media/stonetop.79ea3a2e.gif"
+  },
+  {
+    "revision": "41e3ea7adc1115aaf5d98d160e8d8b33",
+    "url": "/http//alminana.github.io/matplaza.com.ph/static/media/target.41e3ea7a.png"
+  },
+  {
+    "revision": "812c895584e0aafdaa1d784223e07652",
+    "url": "/http//alminana.github.io/matplaza.com.ph/static/media/terracotta.812c8955.jpg"
+  },
+  {
+    "revision": "617454b747ef05e07a0d6ce57d56ba1a",
+    "url": "/http//alminana.github.io/matplaza.com.ph/static/media/terracottacontent.617454b7.gif"
+  },
+  {
+    "revision": "b52131a902c775b42c3c56fca7db74ae",
+    "url": "/http//alminana.github.io/matplaza.com.ph/static/media/terracottatop.b52131a9.gif"
+  },
+  {
+    "revision": "b59180894191dbb1f76adbf788fb7318",
+    "url": "/http//alminana.github.io/matplaza.com.ph/static/media/tile.b5918089.JPG"
+  },
+  {
+    "revision": "f63f0f79394cadc8086f45bd600d67ea",
+    "url": "/http//alminana.github.io/matplaza.com.ph/static/media/tilecontent.f63f0f79.gif"
+  },
+  {
+    "revision": "997b4834cd7d24eb0c21334bedfd2f53",
+    "url": "/http//alminana.github.io/matplaza.com.ph/static/media/tiletop.997b4834.gif"
+  }
+]);
